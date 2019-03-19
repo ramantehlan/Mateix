79f398ad691bbc138fcc6d0d2b15e2d65a695692
raman tehlan <ramantehlan@gmail.com>
2019-03-20 03:15:53 +0530

    CLI started

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+  "github.com/ramantehlan/mateix/packages/error"
+)
 
-func main(){
-  fmt.Println("Hello Mateix")
+func main() {
+
+	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+	updateFilePtr := updateCmd.String("file", "", "File path which you wish to sync")
+
+	// No sub-command
+	if len(os.Args) < 2 {
+		error.Error("Sub command required")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "update":
+		updateCmd.Parse(os.Args[2:])
+	default:
+		error.Error("Unknown sub command")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if updateCmd.Parsed() {
+		if *updateFilePtr == "" {
+      error.Error("Following is the u")
+			updateCmd.PrintDefaults()
+			os.Exit(1)
+		}
+		// Print
+		fmt.Println("You are here")
+	}
+
 }
