394960b37ad82512bd538c25f836be5d0e97932f
raman tehlan <ramantehlan@gmail.com>
2019-03-20 07:00:57 +0530

    init started

--- init.go
+++ init.go
@@ -2,8 +2,14 @@ package main
 
 import (
   "fmt"
+  "os/exec"
+  "github.com/ramantehlan/mateix/packages/command"
 )
 
 func initialize() {
-  fmt.Println("init command to list it in mateixWatch")
+  fmt.Println("init command to list it in mateixWatch " + command.GetCurrentPath() )
+
+  if ! command.FileExist(".mateix") {
+    command.Execute(exec.Command("mkdir", ".mateix"))
+  }
 }
