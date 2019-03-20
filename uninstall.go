85c0d5af671c8e7d1656de08bed24a22a64511b1
raman tehlan <ramantehlan@gmail.com>
2019-03-20 07:01:31 +0530

    Minor changes

--- uninstall.go
+++ uninstall.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"log"
-	"os"
 	"os/exec"
-	"os/user"
   "fmt"
+  "github.com/ramantehlan/mateix/packages/command"
 )
-
-func execute(cmd *exec.Cmd) {
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-}
-
-func getHome() string {
-	usr, err := user.Current()
-	if err != nil {
-		log.Fatal(err)
-	}
-	return usr.HomeDir
-}
 
 func uninstall() {
 	var files = []string{
@@ -32,25 +13,24 @@ func uninstall() {
 		"/etc/systemd/system/multi-user.target.wants/mateix-watch.service",
 		"/etc/systemd/system/mateix-watch.service",
 		"/etc/.mateix",
-		getHome() + "/.mateixConfig",
+		command.GetHome() + "/.mateixConfig",
 	}
 
   fmt.Println("Service stopped")
-  execute(exec.Command("sudo", "mateixWatch", "stop"))
+  command.Execute(exec.Command("sudo", "mateixWatch", "stop"))
 
   for file , _ := range files {
-		fi , err := os.Stat(files[file])
-		if err == nil {
+		if command.FileExist(files[file]) {
+			fi := command.GetStat(files[file])
 			switch mode := fi.Mode(); {
 			case mode.IsDir():
-				execute(exec.Command("sudo", "rm", "-r", files[file]))
+				command.Execute(exec.Command("sudo", "rm", "-r", files[file]))
         fmt.Println("Removed ", files[file])
 			case mode.IsRegular():
-				execute(exec.Command("sudo", "rm", files[file]))
+				command.Execute(exec.Command("sudo", "rm", files[file]))
         fmt.Println("Removed ", files[file])
 			}
 		}
 	}
-
 
 }
