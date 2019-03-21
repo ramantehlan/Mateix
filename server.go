1c4e78951af12691e2558136970374fc8c950f81
raman tehlan <ramantehlan@gmail.com>
2019-03-22 02:50:38 +0530

    shifting from ssh to tcp server

--- server.go
+++ server.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
-	"log"
+	"io/ioutil"
+	"net"
+	"strconv"
 
-	"github.com/gliderlabs/ssh"
+	"github.com/ramantehlan/mateix/packages/command"
+	"github.com/ramantehlan/mateix/packages/e"
 )
 
-func server(action bool) {
-	fmt.Println(action)
-	if action {
-		ssh.Handle(func(s ssh.Session) {
-			io.WriteString(s, fmt.Sprintf("Hello %s\n", s.User()))
-		})
+func listenToClinet(addr string, port int) net.Listener {
+	src := addr + ":" + strconv.Itoa(port)
+	server, err := net.Listen("tcp", src)
+	e.Check(err)
+	fmt.Printf("Listening on %s.\n", src)
+	return server
+}
 
-		log.Println("starting ssh server on port 1248")
-		log.Fatal(ssh.ListenAndServe(":1248", nil))
+func server(action bool) {
+	if action {
+		// Start the server
+		fmt.Println("Starting server")
+		server := listenToClinet("0.0.0.0", 1248)
+		defer server.Close()
+
+		for {
+			conn, err := server.Accept()
+			e.Check(err)
+			go handleConnection(conn)
+		}
 
 	} else {
-		// Code to stop the server
+		// End the server
 	}
+}
+
+func handleConnection(conn net.Conn) {
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
+	scanner := bufio.NewScanner(conn)
+	file := ""
+	for {
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
+		file += scanner.Text() + "\n"
+	}
+	d1 := []byte(file)
+	err := ioutil.WriteFile(command.GetHome()+"/mateixTest/sample2.txt", d1, 0644)
+	e.Check(err)
+	fmt.Println("Client at " + remoteAddr + " disconnected.")
 }
