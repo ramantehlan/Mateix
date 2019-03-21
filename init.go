082bf0fd6afdeec3c79f6520a94b61ac9818a59a
raman tehlan <ramantehlan@gmail.com>
2019-03-22 04:21:23 +0530

    Init working

--- init.go
+++ init.go
@@ -1,17 +1,76 @@
 package main
 
 import (
-  "fmt"
-  "os/exec"
-  "github.com/ramantehlan/mateix/packages/command"
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"os/user"
+	"strings"
+
+	"github.com/ramantehlan/mateix/packages/command"
+	"github.com/ramantehlan/mateix/packages/e"
 )
 
+// Config is the structure of any config file
+type Config struct {
+	TargetIP  string `json:"targetIp"`
+	TargetDir string `json:"entity"`
+}
+
+/*
+"targetUser": "mspp",
+"targetIp": "192.168.43.189",
+"targetDir": "/home/mspp/mateixTest"
+*/
+
+// CreateJSON is to create a json file
+func CreateJSON(config Config, jsonFile string) {
+	file, err := json.MarshalIndent(config, "", " ")
+	e.Check(err)
+	err = ioutil.WriteFile(jsonFile, file, 0644)
+	e.Check(err)
+}
+
+// Initialize the mateix watch folder
 func Initialize() {
-  fmt.Println("init command to list it in mateixWatch " + command.GetCurrentPath() )
+	dotFile := command.GetCurrentPath() + "/.mateix"
+	configFile := dotFile + "/config.json"
+	u, _ := user.Current()
 
-  if ! command.FileExist(".mateix") {
-    command.Execute(exec.Command("mkdir", ".mateix"))
-  }
+	if u.Uid == "0" {
 
+		if !command.FileExist(".mateix") {
+			conf := Config{}
+			fmt.Println("This will initialize mateix watch in this folder:")
+			fmt.Print("Target IP: ")
+			reader := bufio.NewReader(os.Stdin)
+			conf.TargetIP, _ = reader.ReadString('\n')
+			conf.TargetIP = strings.ReplaceAll(conf.TargetIP, "\n", "")
+			fmt.Print("Target Directory: ")
+			conf.TargetDir, _ = reader.ReadString('\n')
+			conf.TargetDir = strings.ReplaceAll(conf.TargetDir, "\n", "")
+
+			dat, err := ioutil.ReadFile("/etc/.mateix/syncList")
+			e.Check(err)
+			text := string(dat) + command.GetCurrentPath() + "\n"
+			syncList := []byte(text)
+			ioutil.WriteFile("/etc/.mateix/syncList", syncList, 0644)
+			command.Execute(exec.Command("mkdir", dotFile))
+			command.Execute(exec.Command("touch", configFile))
+			CreateJSON(conf, configFile)
+
+			fmt.Printf("Added '%s' in '/etc/.mateix/syncList'\n", command.GetCurrentPath())
+			fmt.Println("Created ", dotFile)
+			fmt.Println("Created ", configFile)
+		} else {
+			fmt.Printf("%s is already a mateix watched file\n", command.GetCurrentPath())
+		}
+
+	} else {
+		fmt.Println("You need to root to execute this command")
+	}
 
 }
